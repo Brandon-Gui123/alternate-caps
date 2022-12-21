59dedd564dc8c2173e49d34ba4808f0a1fb70b99
Brandon-Gui123 <34325463+Brandon-Gui123@users.noreply.github.com>
2022-12-21 12:26:52 +0800

    Create unit testing project

---
+++ AlternateCapsTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AlternateCapsTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
