e84e71c0b4597fe0b2ce6eb1be14d80b052488d1
Brandon-Gui123 <34325463+Brandon-Gui123@users.noreply.github.com>
2022-12-23 13:01:57 +0800

    Nest tests in RunningWithoutArguments class
    In both tests, we're not specifying any arguments, so I felt that it's
    better to classify these tests and use a class.

--- AlternateCapsTests/AlternateCapsCLITests.cs
+++ AlternateCapsTests/AlternateCapsCLITests.cs
@@ -4,58 +4,61 @@ namespace AlternateCapsTests
 {
     public class AlternateCapsCLITests
     {
-        [Test, Timeout(10_000)]
-        public void RunWithoutArgs_ProcessFromStandardInput_OutputFormattedText()
+        public class RunningWithouArguments
         {
-            ProcessStartInfo startInfo = new()
+            [Test, Timeout(10_000)]
+            public void ProcessFromStandardInput_OutputFormattedText()
             {
-                FileName = "AlternateCaps.exe",
-                Arguments = null,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true
-            };
+                ProcessStartInfo startInfo = new()
+                {
+                    FileName = "AlternateCaps.exe",
+                    Arguments = null,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true
+                };
 
-            using Process? programProcess = Process.Start(startInfo);
+                using Process? programProcess = Process.Start(startInfo);
 
-            // make sure that the process does exist
-            // else, something is very wrong
-            Assert.That(programProcess, Is.Not.Null);
+                // make sure that the process does exist
+                // else, something is very wrong
+                Assert.That(programProcess, Is.Not.Null);
 
-            programProcess.StandardInput.WriteLine("abcdefg");
-            string expected = "aBcDeFg";
-            string? output = programProcess.StandardOutput.ReadLine();
+                programProcess.StandardInput.WriteLine("abcdefg");
+                string expected = "aBcDeFg";
+                string? output = programProcess.StandardOutput.ReadLine();
 
-            // we're done sending inputs
-            programProcess.StandardInput.Close();
+                // we're done sending inputs
+                programProcess.StandardInput.Close();
 
-            Assert.That(output, Is.Not.Null.And.EqualTo(expected));
-        }
+                Assert.That(output, Is.Not.Null.And.EqualTo(expected));
+            }
 
-        [Test, Timeout(10_000)]
-        public void RunWithoutArgs_CapsFirstOption_ProcessFromStandardInput_OutputFormattedText()
-        {
-            ProcessStartInfo startInfo = new()
+            [Test, Timeout(10_000)]
+            public void CapsFirstOption_ProcessFromStandardInput_OutputFormattedText()
             {
-                FileName = "AlternateCaps.exe",
-                Arguments = "--caps-first",
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true
-            };
+                ProcessStartInfo startInfo = new()
+                {
+                    FileName = "AlternateCaps.exe",
+                    Arguments = "--caps-first",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true
+                };
 
-            using Process? programProcess = Process.Start(startInfo);
+                using Process? programProcess = Process.Start(startInfo);
 
-            // make sure that the process does exist
-            // else, something is very wrong
-            Assert.That(programProcess, Is.Not.Null);
+                // make sure that the process does exist
+                // else, something is very wrong
+                Assert.That(programProcess, Is.Not.Null);
 
-            programProcess.StandardInput.WriteLine("abcdefg");
-            string expected = "AbCdEfG";
-            string? output = programProcess.StandardOutput.ReadLine();
+                programProcess.StandardInput.WriteLine("abcdefg");
+                string expected = "AbCdEfG";
+                string? output = programProcess.StandardOutput.ReadLine();
 
-            // we're done sending inputs
-            programProcess.StandardInput.Close();
+                // we're done sending inputs
+                programProcess.StandardInput.Close();
 
-            Assert.That(output, Is.Not.Null.And.EqualTo(expected));
+                Assert.That(output, Is.Not.Null.And.EqualTo(expected));
+            }
         }
     }
 }
